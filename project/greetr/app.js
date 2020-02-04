e77a49c3a29239f4e3bf300c0b2bacb263abed73
Anthony Eriksen <ajrayid@gmail.com>
2020-02-03 21:21:42 -0700

    Finished!

--- project/greetr/app.js
+++ project/greetr/app.js
@@ -14,7 +14,22 @@
 
 // Greetr should support jQuery.
 
+
+// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
 var g = G$('John', 'Doe');
+
+// use our chainable methods
 g.greet().setLang('es').greet(true).log();
 
-g.HTMLGreeting($("#greeting"))
+// let's use our object on the click of the login button
+$('#login').click(function () {
+
+  // create a new 'Greetr' object (let's pretend we know the name from the login)
+  var loginGrtr = G$('John', 'Doe ');
+
+  // hide the login on the screen
+  $('#loginDiv').hide();
+
+  // fire off an HTML greeting, passing the "#greeting" as the selector aswell as the chosen language, and then log the welcome.
+  loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+})
