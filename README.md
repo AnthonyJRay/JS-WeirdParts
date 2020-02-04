00299c4e1493ef9b76063bb72ef3c0cfa4b256b1
Anthony Eriksen <ajrayid@gmail.com>
2020-02-03 17:40:28 -0700

    Cleaned up file structure. Initializing course project files.

---
+++ README.md
@@ -0,0 +1,9 @@
+# JavaScript: Understanding the Weird Parts
+
+## By: Anthony Alicea
+
+The _Course Code_ folder contains syntax notes and examples from various sections of the course.
+
+The _index.html_ file does not have the files imported into it. Will need to import them as needed for review.
+
+Also inside the _Course Code_ folder is a long file of notes. Some notes verbatim, for me to reference and review at later times as needed.
---
+++ project/greetr/app.js
@@ -0,0 +1,15 @@
+// JavaScript: Understanding the Weird Parts by Anthony Alicea
+// Project: Build your own Framework - "Greetr"
+
+// INSTRUCTIONS
+
+// - When given a first name, last name, and optional language,
+// it generates formal and informal greetings.
+
+// Should support both English and Spanish Languages
+
+// It needs to be Reusable, and not interfere with any other framework or code.
+
+// Should have an easy to type structure. Example: 'G$()' structure.
+
+// Greetr should support jQuery.
